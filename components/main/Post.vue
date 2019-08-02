ed89f54e9848f495fed8bc24785e1bb93978bc36
mikhalovich <zheny.mi@gmail.com>
2019-08-02 14:29:53 +0300

    Added Post component

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,19 +1,61 @@
 <template>
-  <article class="post">
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
 
-  </article>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}</small>
+    </header>
+    <div class="post-body">
+      <img src="https://g2.delphi.lv/images/pix/676x385/oGc34ft8X28/minsk-47394187.jpg" alt="post image" class="post-image">
+    </div>
+
+    <footer class="post-footer">
+      <el-button round @click="openPost">Open post</el-button>
+
+      <span>
+        <i class="el-icon-message"></i>
+        12
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
 export default {
+  methods: {
+    openPost() {
 
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
   .post {
-    height: 200px;
-    border: 1px solid black;
-    margin-bottom: 2rem;
+    margin-bottom: 1.5rem;
+  }
+
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .post-image {
+    width: 100%;
+    height: auto;
+  }
+
+  .post-footer {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 </style>
