758d23985cc6ff2eeec44186339c9ed442424385
mikhalovich <zheny.mi@gmail.com>
2019-08-10 12:16:39 +0300

    Added aside menu

---
+++ layouts/admin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="admin-layout-wrap">
+    <el-container :style="{height: '100%'}">
+      <el-aside width="250px">
+        <app-aside />
+      </el-aside>
+      <el-main>
+        <nuxt />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import AppAside from '@/components/admin/Aside';
+
+export default {
+  components: {
+    AppAside,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .admin-layout-wrap {
+    height: 100vh;
+    width: 100%;
+  }
+</style>
---
+++ pages/admin/list.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>List</div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+}
+</script>
