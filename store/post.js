2370103a0d2af719a9fb78502dd8e95899b03147
mikhalovich <z-m@tut.by>
2019-09-23 23:04:34 +0300

    Added admin post pages functionality

---
+++ store/post.js
@@ -0,0 +1,12 @@
+export const actions = {
+  async fetchAdmin({}) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          {title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: '222'},
+          {title: 'Post 2', date: new Date(), views: 13, comments: [1, 2, 3], _id: '111'}
+        ])
+      })
+    })
+  }
+}
