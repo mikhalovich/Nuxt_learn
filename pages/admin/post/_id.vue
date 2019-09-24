4ecc6fc0d0f5fe104fdc76c003968b7cabf57e67
mikhalovich <z-m@tut.by>
2019-09-24 17:25:56 +0300

    added functionality for editing a post

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -1,15 +1,122 @@
 <template>
-  <div>
-    <el-breadcrumb separator="/">
+  <div class="page-wrap">
+    <el-breadcrumb separator="/" class="mb">
       <el-breadcrumb-item to="/admin/list">Posts</el-breadcrumb-item>
-      <el-breadcrumb-item>{{ $route.params.id }}</el-breadcrumb-item>
+      <el-breadcrumb-item>{{ post.title }}</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <el-form
+      :model="controls"
+      :rules="rules"
+      ref="form"
+      @submit.native.prevent="onSubmit"
+    >
+      <!-- <h2>Login to admin panel</h2> -->
+
+      <el-form-item label="Text format .md or .html" prop="text">
+        <el-input
+          type="textarea"
+          v-model.trim="controls.text"
+          resize="none"
+          :rows="10"
+        />
+      </el-form-item>
+
+      <div class="mb">
+        <small class="mr">
+          <i class="el-icon-time"></i>
+          <span>
+            {{ new Date(post.date).toLocaleString() }}
+          </span>
+        </small>
+
+        <small>
+          <i class="el-icon-view"></i>
+          <span>
+            {{ post.views }}
+          </span>
+        </small>
+      </div>
+
+      <el-form-item>
+        <el-button
+          type="primary"
+          native-type="submit"
+          round
+          :loading="loading"
+        >
+          Refresh
+        </el-button>
+      </el-form-item>
+
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
   layout: 'admin',
-  middleware: ['admin-auth']
+  middleware: ['admin-auth'],
+  head() {
+    return {
+      title: `Post | ${this.post.title}`
+    }
+  },
+
+  validate({params}) {
+    return Boolean(params.id);
+  },
+
+  async asyncData({store, params}) {
+    const post = await store.dispatch('post/fetchAdminById', params.id);
+    return {post};
+  },
+
+  data() {
+    return {
+      loading: false,
+      controls: {
+        text: '',
+      },
+      rules: {
+        text: [
+          { required: true, message: 'Please input text', trigger: 'blur' },
+        ],
+      }
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate( async valid => {
+        if (valid) {
+          this.loading = true;
+
+          const formData = {
+            text: this.controls.text,
+            id: this.post._id,
+          };
+          try {
+            await this.$store.dispatch('post/update', formData);
+            this.$message.success('Post is updated');
+            this.loading = false;
+          } catch (e) {
+            this.loading = false;
+          }
+        }
+      });
+    },
+  },
+
 }
 </script>
+
+<style lang="scss" scoped>
+  .page-wrap {
+    width: 600px;
+  }
+
+  .mr {
+    margin-right: 2rem;
+  }
+</style>
--- store/post.js
+++ store/post.js
@@ -1,16 +1,30 @@
+const posts = [
+  {title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: 'id222'},
+  {title: 'Post 2', date: new Date(), views: 13, comments: [1, 2, 3], _id: 'id111'}
+];
+
 export const actions = {
   async fetchAdmin({}) {
     return await new Promise(resolve => {
       setTimeout(() => {
-        resolve([
-          {title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: '222'},
-          {title: 'Post 2', date: new Date(), views: 13, comments: [1, 2, 3], _id: '111'}
-        ])
+        resolve(posts)
       }, 1000);
-    })
+    });
   },
 
   async remove({}, id) {
 
   },
+
+  async update({}, {id, text}) {
+
+  },
+
+  async fetchAdminById({}, id) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(posts.find(post => post._id === id))
+      }, 1000);
+    });
+  }
 }
