a0520e3dd738ac5516a6b6466c771fba768ab514
mikhalovich <z-m@tut.by>
2019-09-27 17:22:18 +0300

    Added fucntionality for connection to MongoDB database

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const keys = require('./keys')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect(keys.MONGO_URI)
+  .then(() => console.log('MongoDB connected...'))
+    .catch(error => console.error(error))
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+module.exports = app;
+
---
+++ server/keys/keys.dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGO_URI: `mongodb+srv://mikhalovich:Aib3ia7chi@nuxt-blog-1qpav.mongodb.net/test?retryWrites=true&w=majority`
+}
