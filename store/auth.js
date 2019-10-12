83a43749aba4ce3f0ee0d2636b459d2f1373ff49
mikhalovich <z-m@tut.by>
2019-10-12 19:22:39 +0300

    Added autologin and cookies

--- store/auth.js
+++ store/auth.js
@@ -1,4 +1,6 @@
-// import { createSecureServer } from "http2";
+import Cookie from 'cookie'
+import Cookies from 'js-cookie'
+import jwtDecode from 'jwt-decode'
 
 export const state = () => ({
   token: null
@@ -25,16 +27,6 @@ export const actions = {
     }
   },
 
-  setToken({ commit }, token) {
-    this.$axios.setToken(token, 'Bearer');
-    commit('setToken', token);
-  },
-
-  logout({ commit }) {
-    this.$axios.setToken(false);
-    commit('clearToken');
-  },
-
   async createUser({ commit }, formData) {
     try {
       await this.$axios.$post('/api/auth/admin/create', formData)
@@ -43,9 +35,46 @@ export const actions = {
       throw e;
     }
   },
+
+  setToken({ commit }, token) {
+    this.$axios.setToken(token, 'Bearer');
+    commit('setToken', token);
+    Cookies.set('jwt-token', token);
+  },
+
+  logout({ commit }) {
+    this.$axios.setToken(false);
+    commit('clearToken');
+    Cookies.remove('jwt-token');
+  },
+
+  autoLogin({dispatch}) {
+    const cookieStr = process.browser ? document.cookie : this.app.context.req.headers.cookie;
+
+    const cookies = Cookie.parse(cookieStr || '') || {};
+    const token = cookies['jwt-token'];
+
+    if (isJWTValid(token)) {
+      dispatch('setToken', token)
+    } else {
+      dispatch('logout');
+    }
+  }
+
 };
 
 export const getters = {
   isAuthenticated: state => Boolean(state.token),
   token: state => state.token,
+}
+
+function isJWTValid(token) {
+  if (!token) {
+    return false;
+  }
+
+  const  jwtData = jwtDecode(token) || {};
+  const expires = jwtData.exp || 0;
+
+  return (new Date().getTime() / 1000) < expires
 }
